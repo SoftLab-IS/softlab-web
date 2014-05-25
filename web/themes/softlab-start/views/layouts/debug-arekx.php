1ea1b17b92a92020b93f80cbde7b49fe717da6c8
Aleksandar Panic <arekusanda1@gmail.com>
2014-05-26 00:06:11 +0200

    Dodavanje funkcionalnosti u debug skriptu.

--- web/themes/softlab-start/views/layouts/debug-arekx.php
+++ web/themes/softlab-start/views/layouts/debug-arekx.php
@@ -1,5 +1,20 @@
 <?php
  // Debug informacije
+ 
+ // Ovdje dodajes linkove
+ //
+ // Opis:
+ // link -> obican string
+ // kontroler -> array("kontroler/akcija")
+ // kontroler sa GET parametrom -> array("kontroler/akcija", "parametar1" => "1", "parametar2" => "vrednost", ...itd)
+ 
+ $links = array(
+	"home" => Yii::app()->baseUrl . "/index.arekx.php",
+	"contact primjer kontrolera" => array("site/contact"),
+	"contact primjer kontrolera sa parametrima" => array("site/contact", "parametar1" => "1"),
+ );
+ 
+ 
 ?>
 <style type="text/css">
 #debug-info
@@ -13,12 +28,36 @@
 	text-align: center;
 	padding: 10px 0px 10px 0px;
 }
+
+#debug-info p
+{
+	padding: 0px;
+	margin: 0px 0px 5px 0px;
+}
+
+#debug-info a
+{
+	color: #DDDDDD;
+	text-decoration: none;
+}
+
+#debug-info a:hover
+{
+	text-decoration: underline;
+}
 </style>
 <div id="debug-info">
 	
-	<?php echo Yii::app()->name; ?> | 
+	<p><?php echo Yii::app()->name; ?> | 
 
 	Vreme generisanja: <?php echo round(CLogger::getExecutionTime(),3); ?> s |
-	Memorija: <?php echo round(CLogger::getMemoryUsage() / 1048576, 2);?> MB
-
+	Memorija: <?php echo round(CLogger::getMemoryUsage() / 1048576, 2);?> MB | <?php echo CHtml::link("gii", array("gii/default"), array("target" => "_blank")); ?></p>
+	
+	<?php if (count($links) > 0): ?>
+	<p>
+	<?php foreach($links as $key => $val): ?>
+		<?php echo CHtml::link($key, $val); ?> | 
+	<?php endforeach; ?>
+	</p>
+	<?php endif; ?>
 </div>
